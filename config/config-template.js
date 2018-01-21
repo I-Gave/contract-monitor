90deaaf1004c45f3bfbe685255e009e35cf8ed7c
tenthirtyone <Spactigus@gmail.com>
2018-01-21 00:31:17 -0500

    move abi to config

--- config/config-template.js
+++ config/config-template.js
@@ -3,7 +3,6 @@ const config = {
   [
     {
       name: 'I Gave',
-      abi: 'IGVCore.js',
       address: "0x8d80fe98e4b657a8b517e744b4c2a0e73d6a8977",
       rpc: "http://45.76.250.111:9546",
       fromBlock: 0,
